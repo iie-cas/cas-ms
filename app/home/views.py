354ca17a95ce08f7fc9cf053cc48ca8d2de8bf1f
ylhao <765422195@qq.com>
2018-01-24 19:08:04 +0800

    用户修改个人基本信息

--- app/home/views.py
+++ app/home/views.py
@@ -2,7 +2,7 @@
 
 
 from . import home
-from flask import render_template, redirect, url_for, session, request, redirect, flash
+from flask import render_template, redirect, url_for, session, request, flash, jsonify
 from functools import wraps
 from app import getConn
 
@@ -31,29 +31,31 @@ def login():
     用户登录
     """
     if request.method == 'POST':
-        account = request.form['account']
-        password = request.form['password']
-        with getConn() as cursor:
-            cursor.execute(
-                '''
-                select * from user where account="%s"
-                ''' %account
-            )
-        value = cursor.fetchone()
-        if not value:
-            # 判断用户名是否存在
-            flash(u'用户名不存在')
+        account = request.form.get('account')
+        password = request.form.get('password')
+        db = getConn()
+        cursor = db.cursor()
+        sql = '''select id, account, password, username from user where account="%s"''' %account
+        try:
+            value = None
+            cursor.execute(sql)
+            value = cursor.fetchone()
+        except:
+            print('[Error][login]: unable to fetch data')
+        if not value:  # 判断用户是否存在
+            return jsonify({'message': u'用户名不存在'})
         else:
             columns = ('id', 'account', 'password', 'username')
             user = dict(zip(columns, value))
-            if user['password'] == password:
-                # 判断密码是否正确
+            if user['password'] == password:  # 判断密码是否正确
                 for key in user:
                     session[key] = user[key]
-                return redirect(url_for('home.user'))
+                return jsonify({'message': ''})
             else:
-                flash(u'密码错误')
-    return render_template('home/login.html')
+                return jsonify({'message': u'密码错误'})
+        db.close()
+    else:
+        return render_template('home/login.html')
  
 
 @home.route('/logout/')
@@ -68,25 +70,43 @@ def logout():
     # 重定向到登录页面
     return redirect(url_for('home.login'))
 
-
 @home.route('/user/', methods=['GET', 'POST'])
 @home_login_req
 def user():
     if request.method == 'POST':
-        # 如果用户修改了个人信息，会发送 POST 请求 
-        pass
-    # 如果是 GET 请求，则会直接执行以下代码
-    account = session['account']
-    with getConn() as cursor:
-        cursor.execute(
-            '''
-            select id, account, password, username, education, grade, score, fund, telephone, qq from user where account="%s"
-            ''' %account
-        )
-        user = cursor.fetchone()
+        user_id = request.form.get("id")
+        tel = request.form.get("tel")
+        qq = request.form.get("qq")
+        status = True
+        db = getConn()
+        cursor = db.cursor()
+        sql = '''update user set telephone="%s", qq="%s" where id="%s"''' %(tel, qq, user_id)
+        try:
+            cursor.execute(sql)
+            db.commit()
+        except:
+            db.rollback()
+            status = False
+        db.close()
+        return jsonify({"status": status})
+    else:
+        account = session['account']
+        db = getConn()
+        cursor = db.cursor()
+        sql = '''select id, account, password, username, education, grade, score, fund, telephone, qq from user
+                 where account="%s"''' %account
+        try:
+            user = None
+            cursor.execute(sql)
+            user = cursor.fetchone()
+        except:
+            pass
         columns = ('id', 'account', 'password', 'username', 'education', 'grade', 'score', 'fund', 'telephone', 'qq')
         user = dict(zip(columns, user))
-    return render_template('home/user.html', user=user)  
+        for key in user:
+            if user[key] is None:
+                user[key] = u"待录入"
+        return render_template('home/user.html', user=user)  
 
 
 @home.route('/pwd/')
